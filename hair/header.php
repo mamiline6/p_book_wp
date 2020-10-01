dbaf51f4b3c0b67334554612c2585dd90bef2133
mamiline6 <ohmamirineoo6@gmail.com>
2020-10-01 22:05:37 +0900

    2-5 グローバルナビの作成
    数値（1）やテキスト部分を変えても変化が見えなかった

--- hair/header.php
+++ hair/header.php
@@ -12,5 +12,9 @@
             <h1><a href="<?php bloginfo( 'url' ); ?>">
             <img src="<?php bloginfo( 'template_directory' ); ?>/images/logo.png" width="175" height="27" alt="bloginfo( 'name' );">
             </a></h1>
+
+            <ul id="navGlobal">
+                <?php wp_list_pages( array ( 'depth' => 1, 'title_li' => '' ) ); ?>
+            </ul>
         </header>
         <div id="container">
