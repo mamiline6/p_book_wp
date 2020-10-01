adc844af5ff8bdd5f97d3b0cc9a57219359b367b
mamiline6 <ohmamirineoo6@gmail.com>
2020-10-02 00:06:21 +0900

    2-6 アイキャッチ画像と抜粋枠の設定

--- hair/functions.php
+++ hair/functions.php
@@ -3,9 +3,19 @@
  * 月別アーカイブタイトルを日本語表記で返す
  * @return 月別アーカイブ時のタイトル文字列
  */
-
 function single_month_title_jp() {
 	return get_query_var( 'year' )."年".get_query_var( 'monthnum' )."月";
 }
+
+/**
+ * 固定ページに抜粋を表示する
+ */
+ add_post_type_support( 'page', 'excerpt' );
+
+/**
+ * 固定ページにアイキャッチ画像を表示する
+ */
+add_theme_support( 'post-thumbnails', array( 'page' ) );
+set_post_thumbnail_size( 200, 150, true );
 
 ?>
