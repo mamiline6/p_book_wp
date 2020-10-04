7c1d2a6213a8a4c49fc5469b281829232d3cffcf
mamiline6 <ohmamirineoo6@gmail.com>
2020-10-04 14:19:05 +0900

    2-8 存在しないページを404に設定する
    404.php テンプレートファイルを作成する

---
+++ hair/404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<main id="main">
+
+<h1>指定されたページは存在しませんでした。</h1>
+
+	<div class="content">
+		<p>現在表示する記事がありません。</p>
+	</div><!-- .content -->
+
+</main>
+
+<?php get_footer(); ?>
