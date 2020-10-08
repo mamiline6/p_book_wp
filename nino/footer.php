7b116a2bfb6aa7dce5fe7855854d55938dca6dd2
mamiline6 <ohmamirineoo6@gmail.com>
2020-10-08 23:46:05 +0900

    3-2 ヘッダー・フッターの表示
    - common.css からhtml構造を推測したもの

---
+++ nino/footer.php
@@ -0,0 +1,13 @@
+<div class="pagetop"><a href="#"></a></div>
+    </div></div><!-- /#container -->
+
+    <footer id="footer">
+        <h1></h1>
+        <dl>
+            <dt></dt>
+            <dd></dd>
+        </dl>
+    </footer><!-- /#footer -->    
+</div><!-- /#wrapper -->
+</body>
+</html>
---
+++ nino/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
+    <title><?php wp_title( '|', true, 'right' ); ?><?php bloginfo( 'name' ); ?></title>
+</head>
+<body <?php body_class(); ?>>
+<div id="wrapper">
+	<header id="header"><div id="headerInner">
+        <h1></h1>
+        <p></p>
+        <div id="navGloval">
+            <ul>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+            </ul>
+        </div><!-- /#navGloval -->
+    </div></header><!-- /#header -->
+
+    <div id="container"><div id="containerInner">
