4aefb0a461e21d53dd53b6cbc1f1df1ce197f39e
mamiline6 <ohmamirineoo6@gmail.com>
2020-10-15 00:10:09 +0900

    3-6 分類タイプリストの表示とフィルターフック

--- nino/functions.php
+++ nino/functions.php
@@ -2,61 +2,80 @@
 /**
  * カスタム投稿、カスタム分類タイプを追加
  */
-function add_custom () {
-	register_taxonomy ( 'cat_menu',           // カスタムタクソノミー名
+function add_custom() {
+	register_taxonomy( 'cat_menu',           // カスタムタクソノミー名
 	'post_menu',                              // このタクソノミーが使われるカスタム投稿タイプ（ post_menu に属する）
-	array (
+	array(
 		'label'        => 'メニューカテゴリー',  // カスタムタクソノミーのラベル名（管理画面上）
 		'show_ui'      => true,               // 管理画面に表示する
 		'show_in_rest' => true,               // Gutenberg（REST API）を有効化
 		'hierarchical' => true,               // 分類に親子関係を持たせる
-		'rewrite'      => array (             // アーカイブページの URL を /menu/category/ スラッグとする
+		'rewrite'      => array(             // アーカイブページの URL を /menu/category/ スラッグとする
 			'slug' => 'menu/category',
 			'with_front' => false
 		)
 	));
 
-	register_post_type ( 'post_menu',        // カスタム投稿タイプ名 
-	array (
+	register_post_type( 'post_menu',        // カスタム投稿タイプ名 
+	array(
 		'label'         => 'メニュー',        // カスタム投稿タイプのラベル名（管理画面上）
 		'menu_position' => 5,                // 管理画面の表示位置
 		'public'        => true,             // 管理画面に表示しサイト上にも表示する
 		'has_archive'   => false,            // アーカイブページは持たない（カスタム分類のアーカイブで表示する）
 		'show_in_rest'  => true,             // Gutenberg（REST API）を有効化
-		'rewrite'       => array (           // シングルページの URL を /menu/ スラッグとする
+		'rewrite'       => array(           // シングルページの URL を /menu/ スラッグとする
 			'slug' => 'menu',
 			'with_front' => false
 		),
-		'supports'      => array (    // タイトル、内容、アイキャッチ画像、カスタムフィールドを使う
+		'supports'      => array(    // タイトル、内容、アイキャッチ画像、カスタムフィールドを使う
 			'title', 'editor', 'thumbnail', 'custom-field'
 		),
 		'exclude_from_search' => false
 	));
 }
-add_action ( 'init', 'add_custom' );   // init アクションフックで登録
+add_action( 'init', 'add_custom' );   // init アクションフックで登録
 
 /**
  * アイキャッチ画像を設定追加する
  */
-add_theme_support ( 'post-thumbnails', array ( 'post_menu' ) );
-set_post_thumbnail_size ( 450, 450, false );         // カスタム投稿「メニュー」シングルページ用
-add_image_size ( 'menu_thumbnail', 300, 225, true ); // 「メニューカテゴリ」タクソノミーページ用
-add_image_size ( 'top_thumbnail', 150, 150, true );  // フロントページ用
-// add_image_sise ( 'size_thumbnail', 100, 100, true ); // サイドバー用
+add_theme_support( 'post-thumbnails', array( 'post_menu' ) );
+set_post_thumbnail_size( 450, 450, false );         // カスタム投稿「メニュー」シングルページ用
+add_image_size( 'menu_thumbnail', 300, 225, true ); // 「メニューカテゴリ」タクソノミーページ用
+add_image_size( 'top_thumbnail', 150, 150, true );  // フロントページ用
+// add_image_sise( 'size_thumbnail', 100, 100, true ); // サイドバー用
 
 /**
  * カスタムメニューを表示
  */
-add_theme_support ( 'menus' );
+add_theme_support( 'menus' );
 
 /**
  * タクソノミーページ「メニューカテゴリ」の表示順を投稿の昇順に変更する
  */
-function reverce_tax_menu ( $wp_query ) {
-	if ( is_main_query () && is_tax ( 'cat_menu' ) ) {
-		$wp_query -> set ( 'order', 'ASC' );
+function reverce_tax_menu( $wp_query ) {
+	if( is_main_query () && is_tax( 'cat_menu' ) ) {
+		$wp_query -> set( 'order', 'ASC' );
 	}
 }
-add_action ( 'pre_get_posts', 'reverce_tax_menu' );
+add_action( 'pre_get_posts', 'reverce_tax_menu' );
+
+/**
+ * カテゴリー名からソートのために付与した文字列を削除する
+ */
+function replace_category_name( $cat_name ) {
+	global $post;
+	// 管理画面では実行しない
+	if( is_admin() ) return $cat_menu;
+		// メニュー以外の投稿タイプでは実行しない
+		if( get_post_type( $post -> ID ) != 'post_menu' ) return $cat_name;
+			// 1:カテゴリ名形式の1:の部分を削除する
+			$split = preg_split( "/:/", $cat_name );
+			if( $split[1] != '' ) {
+				return $split[1];
+			}
+			return $cat_name;
+}
+add_filter( 'list_cats', 'replace_category_name' );
+
 
 ?>
--- nino/taxonomy.php
+++ nino/taxonomy.php
@@ -5,12 +5,18 @@
 	<p style="padding:10px 20px; color: #f00; border: 1px solid; margin: 10px 0 30px; background-color: #fe0;">投稿タイプ「メニュー」のタクソノミーテンプレート taxonomy.php を返して表示しています。</p>
 
 	<ul class="menuCategory">
-		<?php wp_nav_menu ( array (
-			'menu' => 'subnav'
-		) ); ?>
+	<?php wp_list_categories ( array (
+		'menu'     => 'subnav',
+		'title_li' => '',
+		'taxonomy' => 'cat_menu',
+		'orderby'  => 'name',
+		'order'    => 'ASC',
+		'exclude'  => 22
+	)); ?>
 	</ul>
 
 	<div id="menuList">
+
 	<?php // メニューの一覧を出力するループ ?>
 	<?php if ( have_posts() ) : ?>
 
@@ -18,17 +24,17 @@
 		<?php // メニューを出力 ?>
 		<div class="menuItem">
 			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-			<?php // アイキャッチ画像を出力 ?>
 			<a href="<?php the_permalink(); ?>"><?php if ( has_post_thumbnail() ) the_post_thumbnail( 'menu_thumbnail' ); ?>
 			</a>
-			<?php if ( post_custom ( 'PRICE' ) ) : ?>
-				<p class="price">PRICE &yen;<?php echo esc_html ( post_custom ( 'PRICE' ) ); ?></p>
+			<?php if ( post_custom( 'PRICE' ) ) : ?>
+			<p class="price">PRICE &yen;<?php echo esc_html( post_custom( 'PRICE' ) ); ?></p>
 			<?php endif; ?>
 		</div><!-- /.menuItem -->
 		<?php endwhile; ?>
 	<?php else: ?>
 		<p>現在表示するメニューはありません。</p>
 	<?php endif; ?>
+
 </main>
 
 <?php get_sidebar( 'blog' ); ?>
