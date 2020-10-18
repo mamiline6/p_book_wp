557f25b5f82517224306423695ce63225b2d090a
mamiline6 <ohmamirineoo6@gmail.com>
2020-10-18 12:10:54 +0900

    3-9 ウィジェットを配置するサイドバーの登録

--- nino/sidebar-blog.php
+++ nino/sidebar-blog.php
@@ -1,27 +1,6 @@
 <div id="side">
 	<div class="sideNav">
-		<h2>カテゴリ</h2>
-		<ul>
-			<?php wp_list_categories( 'title_li=' ); ?>
-		</ul>
-	</div><!-- .sideNav -->
-	<div class="sideNav">
-		<h2>アーカイブ</h2>
-		<ul>
-			<?php wp_get_archives(); ?>
-		</ul>
-	</div><!-- .sideNav -->
-	<div class="sideNav">
-		<h2>新着ブログ</h2>
-		<?php
-			$newposts = array(
-			'type' => 'postbypost',
-			'limit' => 5
-			);
-			wp_get_archives($newposts);
-			?>
-	</div><!-- .sideNav -->
-	<div class="sideNav">
-		<h2>新着コメント</h2>
+		<?php dynamic_sidebar( 'Blog Sidebar' ); ?>
+		<!-- テンプレートタグにより出力していたアーカイブリンクなどの記述を削除 -->
 	</div><!-- .sideNav -->
 </div>
