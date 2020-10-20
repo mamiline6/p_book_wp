48e78b10b196864edc23733e4837be35371d8053
mamiline6 <ohmamirineoo6@gmail.com>
2020-10-20 14:34:55 +0900

    Part4 トップページをテンプレート化する
    - 独自タグ：wp_widget_num_class→functions.phpを確認

--- miraggio/index.php
+++ miraggio/index.php
@@ -6,71 +6,39 @@
 <section id="contents">
 <!--/* ▲ #contents 開始 */-->
 
-<section id="home-widget-area" class="widget-area widgets-3">
-	<aside class="widget-container">
-		<h1 class="widget-title">日替わりイタリアンスイーツ</h1>
-		<div class="textwidget">
-		<p><img src="img/dummy/home03.png" alt="">
-		旬の有機野菜を使った７月までの季節限定の冷製パスタです。
-		<a href="#" class="link-btn">詳細はこちら</a></p>
-		</div>
-	</aside>
-
-	<aside class="widget-container">
-		<h1 class="widget-title">初夏の有機野菜冷製パスタ</h1>
-		<div class="textwidget">
-		<p><img src="img/dummy/home01.png" alt="">
-		本場イタリア仕込みのスイーツを日替わりでご用意しています。
-		<a href="#" class="link-btn">詳細はこちら</a></p>
-		</div>
-	</aside>
-
-	<aside class="widget-container">
-		<h1 class="widget-title">オーガニックブレンド</h1>
-		<div class="textwidget">
-		<p><img src="img/dummy/home02.png" alt="">
-		Cafe Miraggio がこだわり抜いた特製オーガニックブレンド。
-		<a href="#" class="link-btn">詳細はこちら</a></p>
-		</div>
-	</aside>
+<?php if(is_active_sidebar('home-content-widget-upper')): ?>
+<section id="home-widget-area" class="<?php wp_widget_num_class('home-content-widget-upper'); ?>">
+	<?php dynamic_sidebar('home-content-widget-upper'); ?>
 </section>
+<?php endif; ?>
 
 <section id="update-info">
+<?php 
+if(! dynamic_sidebar('home-content-widget-lowwer')):
+	if(have_posts()):
+?>
 	<h1 class="update-info-title">information</h1>
 	<ul>
+	<?php while(have_posts()): the_post(); ?>
 		<li>
-			<a href="#">
-				<img src="img/default/update-info-thumbnail-default.png" alt="">
+			<a href="<?php the_permalink(); ?>">
+			<?php 
+			if(has_post_thumbnail()):
+				the_post_thumbnail();
+			else:
+				echo '<img src="'.get_default_image('default/update-info-thumbnail-default.png').'" alt="">';
+			endif; ?>
 			</a>
-			<h2 class="update-title"><a href="#">コーヒーと植物のある生活。</a></h2>
+			<h2 class="update-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 			<p class="update-meta">
-				<span class="entry-date"><a href="#"><time datetime="2011-06-08">2011.06.08</time></a></span>
-				<span class="entry-category"><a href="#">Staff blog</a></span>
+				<span class="entry-date"><a href="<?php the_permalink(); ?>"><time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y.m.d'); ?></time></a></span>
+				<span class="entry-category"><?php the_category(','); ?></span>
 			</p>
 		</li>
-
-		<li>
-			<a href="#">
-				<img src="img/default/update-info-thumbnail-default.png" alt="">
-			</a>
-			<h2 class="update-title"><a href="#">新ランチメニューのお知らせです。</a></h2>
-			<p class="update-meta">
-				<span class="entry-date"><a href="#"><time datetime="2011-06-08">2011.06.08</time></a></span>
-				<span class="entry-category"><a href="#">Staff blog</a></span>
-			</p>
-		</li>
-
-		<li>
-			<a href="#">
-				<img src="img/default/update-info-thumbnail-default.png" alt="">
-			</a>
-			<h2 class="update-title"><a href="#">美術展に行ってきました！</a></h2>
-			<p class="update-meta">
-				<span class="entry-date"><a href="#"><time datetime="2011-06-08">2011.06.08</time></a></span>
-				<span class="entry-category"><a href="#">Staff blog</a></span>
-			</p>
-		</li>
+	<?php endwhile; ?>
 	</ul>
+	<?php endif; ?>
+<?php endif; ?>
 </section>
 <!--/* ▼ #contents 終了 */-->
 </section>
