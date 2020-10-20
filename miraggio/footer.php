8f09619db2990871ce5325430540ea3cc415e784
mamiline6 <ohmamirineoo6@gmail.com>
2020-10-20 14:33:41 +0900

    Part4-Step6 footer.php のテンプレート化
    - 独自タグ：is_active_sidebar_area, wp_area_widget_num_class→functions.phpを確認

--- miraggio/footer.php
+++ miraggio/footer.php
@@ -1,59 +1,61 @@
 <footer id="footer">
+
+<?php
+$area_widgets = array(
+	'first-footer-widget-area',
+	'second-footer-widget-area',
+	'third-footer-widget-area',
+	'fourth-footer-widget-area'
+);
+
+if('is_active_sidebar_area($area_widgets)'):
+?>
 <!--/* ▲ #footer 開始 */-->
-	<div id="footer-widget-area" class="area-widgets-4">
+	<div id="footer-widget-area" class="area-widgets <?php wp_area_widget_num_class($area_widgets); ?>">
 	<!--/* ▲ #footer-widget-area 開始 */-->
+
+		<?php if(is_active_sidebar('first-footer-widget-area')): ?>
 		<section id="first" class="widget-area">
-			<aside class="widget-container">
-				<h1 class="widget-title">widget title</h1>
-				<div class="textwidget">
-					テキストテキストテキストテキストテキストテキストテキストテキスト
-				</div>
-			</aside>
+			<?php dynamic_sidebar('first-footer-widget-area'); ?>
 		</section>
+		<?php endif; ?>
 
+		<?php if(is_active_sidebar('second-footer-widget-area')): ?>
 		<section id="second" class="widget-area">
-			<aside class="widget-container">
-				<h1 class="widget-title">widget title</h1>
-				<div class="textwidget">
-					テキストテキストテキストテキストテキストテキストテキストテキスト
-				</div>
-			</aside>
+			<?php dynamic_sidebar('second-footer-widget-area'); ?>
 		</section>
+		<?php endif; ?>
 
+		<?php if(is_active_sidebar('third-footer-widget-area')): ?>
 		<section id="third" class="widget-area">
-			<aside class="widget-container">
-				<h1 class="widget-title">widget title</h1>
-				<div class="textwidget">
-					テキストテキストテキストテキストテキストテキストテキストテキスト
-				</div>
-			</aside>
+			<?php dynamic_sidebar('third-footer-widget-area'); ?>
 		</section>
+		<?php endif; ?>
 
+		<?php if(is_active_sidebar('fourth-footer-widget-area')): ?>
 		<section id="fourth" class="widget-area">
-			<aside class="widget-container">
-				<h1 class="widget-title">widget title</h1>
-				<div class="textwidget">
-					テキストテキストテキストテキストテキストテキストテキストテキスト
-				</div>
-			</aside>
+			<?php dynamic_sidebar('fourth-footer-widget-area'); ?>
 		</section>
+		<?php endif; ?>
 
 	<!--/* ▼ #footer-widget-area 終了 */-->
 	</div>
+<?php endif; ?>
 
-	<nav id="footer-nav">
-		<ul>
-			<li><a href="#">contact</a></li>
-			<li><a href="#">link</a></li>
-			<li><a href="#">recruit</a></li>
-			<li><a href="#">sitemap</a></li>
-		</ul>
-	</nav>
+	<?php wp_nav_menu(
+	array(
+		'container'      => 'nav',
+		'container_id'   => 'footer-nav',
+		'theme_location' => 'footer',
+		'fallback_cd'    => null
+
+	)
+); ?>
 	<p id="copyright">
-		<small>Copyright &copy; 2012 <a href="#">Cafe Miraggio</a> All rights reserved.</small>
+		<small>Copyright &copy; <?php echo date_i18n('Y'); ?> <a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a> All rights reserved.</small>
 	</p>
 	<p id="go-top"><a href="#header" onclick="scrollup(); return false;">
-		<img src="img/go-top.png" alt="go top">
+		<img src="<?php echo get_default_image('go-to.png'); ?>" alt="go top">
 	</a></p>
 <!--/* ▼ #footer 終了 */-->
 </footer>
@@ -64,6 +66,7 @@
 <!--/* ▼ #wrap 終了 */-->
 </div>
 
-<script src="js/scroll.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/scroll.js"></script>
+<?php wp_footer(); ?>
 </body>
 </html>
