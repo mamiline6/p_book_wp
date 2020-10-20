f1f25595a735c6a0fc5bb28d72fa483a59706c59
mamiline6 <ohmamirineoo6@gmail.com>
2020-10-20 14:32:50 +0900

    Part4-step5 sidebar.php のテンプレート化

--- miraggio/sidebar.php
+++ miraggio/sidebar.php
@@ -1,32 +1,30 @@
 <section id="sidebar">
 <!--/* ▲ #sidebar 開始 */-->
 <div id="primary" class="widget-area">
-	<aside class="widget-container">
-		<h1 class="widget-title">Cafe Miraggio</h1>
-		<div class="textwidget">
-			<p>東京都新宿区市谷左内町21-13<br />TEL 03-1234-5678</p>
+	<?php if(!dynamic_sidebar('primary-widget-area')): ?>
+	<aside id="pages" class="widget-container">
+		<h3 class="widget-title">Menu</h3>
 			<ul>
-				<li>日・火～木 11:00～18:00<br />（ラストオーダー 17:00）</li>
-				<li>金 ・土  11:00～21:00<br />（ラストオーダー19:30)</li>
-				<li>※月曜定休日（祝日の場合、翌日）</li>
+				<?php wp_list_pages('title_li=&depth=2'); ?>
 			</ul>
-		<a href="shop-info/"><img width="170" height="120" alt="アクセスマップ" src="http://maps.google.com/maps/api/staticmap?center=35.692664,139.735949&amp;zoom=15&amp;size=170x120&amp;markers=size:small|35.693465,139.735638&amp;sensor=false"></a>
-		<a href="#" class="twitter-btn">follow</a>
-		</div>
 	</aside>
+	<aside id="archives" class="widget-container">
+		<h3 class="widget-title">Archives</h3>
+		<ul>
+			<?php wp_get_archives('type=monthly'); ?>
+		</ul>
+	</aside>
+	<?php endif; ?>
 </div>
 
+<?php if(is_active_sidebar('secondary-widget-area')): ?>
 <div id="secondary" class="widget-area">
 	<aside class="widget-container">
 		<div class="textwidget">
-			<ul id="banner-list">
-				<li><a href="http://gihyo.jp/book" target="_blank"><img src="img/banner/gihyo.png" alt=""></a></li>
-				<li><a href="http://ja.wordpress.org/" target="_blank"><img src="img/banner/WordPress.png" alt=""></a></li>
-				<li><a href="http://wordbench.org/" target="_blank"><img src="img/banner/WordBench.png" alt="WordBench"></a></li>
-				<li><a href="http://www.w3.org/html/logo/"><img src="http://www.w3.org/html/logo/badge/html5-badge-h-css3-semantics.png" width="165" height="64" alt="HTML5 Powered with CSS3 ／ Styling, and Semantics" title="HTML5 Powered with CSS3 ／ Styling, and Semantics"></a></li>
-			</ul>
+			<?php dynamic_sidebar('secondary-widget-area'); ?>
 		</div>
 	</aside>
 </div>
+<?php endif; ?>
 <!--/* ▼ #sidebar 終了 */-->
 </section>
