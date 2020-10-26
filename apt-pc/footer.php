839e640e14ea45a33f8b2c7454ab8287a5b0b7de
mamiline6 <ohmamirineoo6@gmail.com>
2020-10-27 00:18:09 +0900

    Chapter 2 基本設定とデータインポート まで

---
+++ apt-pc/footer.php
@@ -0,0 +1,46 @@
+</div><!-- #wrap end -->
+<div id="footer-container">
+  <div id="footer">
+    <div class="footer_menu">
+      <div class="footerBlock widget_nav_menu">
+        <h2><a href="#">ツアー情報</a></h2>
+        <ul>
+          <li><a href="#">ハワイ・グアム</a></li>
+          <li><a href="#">ヨーロッパ</a></li>
+          <li><a href="#">日本</a></li>
+          <li><a href="#">アジア</a></li>
+        </ul>
+      </div>
+      <div class="footerBlock widget_nav_menu">
+        <h2><a href="#">営業所</a></h2>
+        <ul>
+          <li><a href="#">大手町営業所</a></li>
+          <li><a href="#">大阪営業所</a></li>
+        </ul>
+      </div>
+      <div class="footerBlock widget_nav_menu">
+        <h2><a href="#">お知らせ</a></h2>
+        <ul>
+          <li><a href="#">IR情報</a></li>
+          <li><a href="#">ニュース</a></li>
+          <li><a href="#">リリース</a></li>
+          <li><a href="#">採用情報</a></li>
+        </ul>
+      </div>
+      <div class="footerBlock widget_nav_menu">
+        <h2><a href="#">会社概要</a></h2>
+        <ul>
+          <li><a href="#">事業紹介</a></li>
+          <li><a href="#">沿革</a></li>
+          <li><a href="#">個人情報保護方針</a></li>
+        </ul>
+      </div>
+    </div><!-- #footer_menu end -->
+    <p id="copyright">
+      <small>Copyright &copy; <?php bloginfo( 'name' ); ?> All rights reserved.</small>
+    </p>
+  </div><!-- #footer end -->
+</div><!-- #footer-container end -->
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ apt-pc/index.php
@@ -0,0 +1,164 @@
+<?php get_header(); ?>
+  <div class="main_image">
+    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/top/main_image.png" width="950" height="329" alt="想像以上の旅をご提供いたします。" /></a>
+  </div><!-- .main_image end -->
+  <div id="container">
+    <div id="main" role="main">
+      <div id="content">
+        <div id="tour_info" class="clearfix">
+          <h2><img src="<?php echo get_template_directory_uri(); ?>/images/top/ttl_tour.png" width="183" height="25" alt="最新ツアー情報"></h2>
+          <p class="list_link"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/top/btn_linklist.png" width="80" height="29" alt="一覧を見る"></a></p>
+          <div class="inner clearfix">
+            <div class="tour_column">
+              <h3><a href="#">ハワイラグジュアリーステイ3日間</a></h3>
+              <p class="tour_info_top">
+                <span class="tour_category hawaii-guam">ハワイ・グアム</span>
+                <span class="tour_date">設定期間：2014/6/16&#xFF5E;2014/9/30</span>
+              </p>
+              <p class="tour_image"><a href="#"><img width="208" height="138" src="<?php echo get_template_directory_uri(); ?>/images/top/t1main-208x138.jpg" alt="ハワイラグジュアリーステイ3日間" /></a></p>
+              <dl class="tour_info_foot clearfix">
+                <dt>旅行代金</dt>
+                <dd>120,000円&#xFF5E;216,000円</dd>
+                <dt>訪問都市</dt>
+                <dd>ホノルル（オアフ島）</dd>
+                <dt>出発地</dt>
+                <dd>成田, 名古屋, 札幌</dd>
+                <dt>シートクラス</dt>
+                <dd>ファーストクラス</dd>
+              </dl>
+            </div>
+            <div class="tour_column">
+              <h3><a href="#">バルセロナ～美術遺産の宝庫を旅する8日間</a></h3>
+              <p class="tour_info_top">
+                <span class="tour_category europe">ヨーロッパ</span>
+                <span class="tour_date">設定期間：2014/6/16&#xFF5E;2014/12/20</span>
+              </p>
+              <p class="tour_image"><a href="#"><img width="208" height="138" src="<?php echo get_template_directory_uri(); ?>/images/top/t7main-208x138.jpg" alt="バルセロナ～美術遺産の宝庫を旅する" /></a></p>
+              <dl class="tour_info_foot clearfix">
+                <dt>旅行代金</dt>
+                <dd>184,000円&#xFF5E;331,200円</dd>
+                <dt>訪問都市</dt>
+                <dd>バルセロナ（スペイン）</dd>
+                <dt>出発地</dt>
+                <dd>成田</dd>
+                <dt>シートクラス</dt>
+                <dd>ビジネスクラス</dd>
+              </dl>
+            </div>
+            <div class="tour_column">
+              <h3><a href="#">中国茶葉と小籠包を求めて～台湾5日間の旅</a></h3>
+              <p class="tour_info_top">
+                <span class="tour_category asia">アジア</span>
+                <span class="tour_date">設定期間：2014/6/25&#xFF5E;2014/11/30</span>
+              </p>
+              <p class="tour_image"><a href="#"><img width="208" height="138" src="<?php echo get_template_directory_uri(); ?>/images/top/t16main-208x138.jpg" alt="中国茶葉と小籠包を求めて～台湾5日間の旅" /></a></p>
+              <dl class="tour_info_foot clearfix">
+                <dt>旅行代金</dt>
+                <dd>160,000円&#xFF5E;288,000円</dd>
+                <dt>訪問都市</dt>
+                <dd>台北</dd>
+                <dt>出発地</dt>
+                <dd>成田, 名古屋, その他</dd>
+                <dt>シートクラス</dt>
+                <dd>エコノミークラス</dd>
+              </dl>
+            </div>
+            <div class="tour_column">
+              <h3><a href="#">大自然に包まれて～北海道を旅する4日間</a></h3>
+              <p class="tour_info_top">
+                <span class="tour_category japan">日本</span>
+                <span class="tour_date">設定期間：2014/5/25&#xFF5E;2014/9/30</span>
+              </p>
+              <p class="tour_image"><a href="#"><img width="208" height="138" src="<?php echo get_template_directory_uri(); ?>/images/top/t12main-208x138.png" alt="大自然に包まれて～北海道を旅する4日間" /></a></p>
+              <dl class="tour_info_foot clearfix">
+                <dt>旅行代金</dt>
+                <dd>46,000円&#xFF5E;82,800円</dd>
+                <dt>訪問都市</dt>
+                <dd>富良野、美瑛</dd>
+                <dt>出発地</dt>
+                <dd>羽田, 名古屋</dd>
+                <dt>シートクラス</dt>
+                <dd>エコノミークラス</dd>
+              </dl>
+            </div>
+            <div class="tour_column">
+              <h3><a href="#">グアムの人気高級ホテルでくつろぐ～ゆったり3日間</a></h3>
+              <p class="tour_info_top">
+                <span class="tour_category hawaii-guam">ハワイ・グアム</span>
+                <span class="tour_date">設定期間：2014/5/10&#xFF5E;2014/12/20</span>
+              </p>
+              <p class="tour_image"><a href="#"><img width="208" height="138" src="<?php echo get_template_directory_uri(); ?>/images/top/t3main-208x138.jpg" alt="グアムの人気高級ホテルでくつろぐ～ゆったり3日間" /></a></p>
+              <dl class="tour_info_foot clearfix">
+                <dt>旅行代金</dt>
+                <dd>120,000円&#xFF5E;216,000円</dd>
+                <dt>訪問都市</dt>
+                <dd>グアム</dd>
+                <dt>出発地</dt>
+                <dd>羽田, 札幌, 名古屋</dd>
+                <dt>シートクラス</dt>
+                <dd>ビジネスクラス</dd>
+              </dl>
+            </div>
+            <div class="tour_column">
+              <h3><a href="#">イタリア周遊５日間、ローマ、ミラノ、ヴェネツィアの旅</a></h3>
+              <p class="tour_info_top">
+                <span class="tour_category europe">ヨーロッパ</span>
+                <span class="tour_date">設定期間：2014/7/18&#xFF5E;2014/12/10</span>
+              </p>
+              <p class="tour_image"><a href="#"><img width="208" height="138" src="<?php echo get_template_directory_uri(); ?>/images/top/t6main-208x138.jpg" alt="イタリア周遊5日間" /></a></p>
+              <dl class="tour_info_foot clearfix">
+                <dt>旅行代金</dt>
+                <dd>200,000円&#xFF5E;360,000円</dd>
+                <dt>訪問都市</dt>
+                <dd>ローマ、ミラノ、ヴェネツィア</dd>
+                <dt>出発地</dt>
+                <dd>成田, 大阪</dd>
+                <dt>シートクラス</dt>
+                <dd>ビジネスクラス</dd>
+              </dl>
+            </div>
+          </div><!-- end .inner -->
+        </div><!-- end #tourInfo -->
+        <div id="top_info" class="clearfix">
+          <h2><img src="<?php echo get_template_directory_uri(); ?>/images/top/ttl_news.png" width="183" height="25" alt="お知らせ"></h2>
+          <p class="list_link"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/top/btn_linklist.png" width="80" height="29" alt="一覧を見る"></a></p>
+          <div class="inner clearfix">
+            <ul>
+              <li>
+                <span class="info_li_inner">
+                  <span class="news_date">2013年04月23日</span>
+                  <span class="news_category ir">IR情報</span>
+                  <a href="#">2014年4月期 第3四半期報告書</a>
+                </span>
+              </li>
+              <li>
+                <span class="info_li_inner">
+                  <span class="news_date">2013年04月23日</span>
+                  <span class="news_category news">ニュース</span>
+                  <a href="#">「アジアこども写真展」を開催します。</a>
+                </span>
+              </li>
+              <li>
+                <span class="info_li_inner">
+                  <span class="news_date">2013年04月23日</span>
+                  <span class="news_category release">リリース</span>
+                  <a href="#">「青春アジア特急」（学生向けパス）がご利用可能となります。</a>
+                </span>
+              </li>
+              <li>
+                <span class="info_li_inner">
+                  <span class="news_date">2013年04月21日</span>
+                  <span class="news_category release">リリース</span>
+                  <a href="#">「豪華客船で行く～インドネシアの島めぐり」予約開始しました。</a>
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div><!-- end #topInfo -->
+      </div><!-- end #content -->
+    </div><!-- end #main -->
+
+  <?php get_sidebar(); ?>
+
+  </div><!-- end #container -->
+<?php get_footer(); ?>
